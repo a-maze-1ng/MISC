f924d89d366e35751263f5c6ab3675df467f7447
a-maze-1ng <105156333+a-maze-1ng@users.noreply.github.com>
2023-01-26 13:35:29 +0100

    Update 7zip via command line.bat

--- 7zip via command line.bat
+++ 7zip via command line.bat
@@ -12,6 +12,9 @@ cd C:\Program Files\7-Zip
 :: -r = Recurse subdirectories
 :: 7z a -tzip -r "DESTINATION.zip" "SOURCE"
 :: #########################################
+:: example:
+:: 7z a -tzip -r "C:\Users\USER\Documents\Backup.zip" "C:\Users\USER\Desktop\"
+:: #########################################
 
 7z a -tzip -r "C:\Users\XYZ\Desktop\files.zip" "C:\Users\XYZ\Desktop\IMPORTANT_DIR_OR_FILE"
 
