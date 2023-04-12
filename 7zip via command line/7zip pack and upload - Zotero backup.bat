c981abb7cf6126eb166c04b8fa375be03ca8f053
a-maze-1ng <105156333+a-maze-1ng@users.noreply.github.com>
2023-04-12 18:48:41 +0200

    Update 7zip pack and upload - Zotero backup.bat

--- 7zip via command line/7zip pack and upload - Zotero backup.bat
+++ 7zip via command line/7zip pack and upload - Zotero backup.bat
@@ -1,14 +1,48 @@
 :: #########################################
-:: set variable
+:: user input
 :: #########################################
 @echo off
 echo set drive letter of the cloude storage (e.g. "G")
 set /p cloud_storage=
+echo:
+
+echo set actual date (e.g. "2023-04-12")
+set /p current_date=
+echo:
+
+:: #########################################
+:: variables
+:: #########################################
+
+:: destination within the cloud
+set cloud_path=:\Meine Ablage\#003 - Zotero
+
+:: \* at path end to ensure that all data in this folder is packed
+set zotero_source=C:\Users\1\Zotero\*
+
+:: pack zotero_source to a zip with current date; save the zip in a buffer location e.g. Documents
+set zip_buffer_location=C:\Users\1\Documents\000 - Zotero Backup\%current_date% - Zotero.zip
+
+echo folder %zotero_source% will be packed as .zip
+echo .zip will be saved in %zip_buffer_location%
+echo .zip will be copied to %cloud_storage%%cloud_path%
+echo .zip will be deleted
+echo:
+
+:: #########################################
+:: choice to abort the script
+:: #########################################
+
+echo run the script? j/n
+set /p wahl=
+if '%wahl%'=='j' goto ja
+if '%wahl%'=='n' goto nein
+
+:ja
 
 :: #########################################
 :: change to the 7-zip install directory
 :: #########################################
-
 cd C:\Program Files\7-Zip
 
 :: #########################################
@@ -23,8 +57,7 @@ cd C:\Program Files\7-Zip
 :: Source: C:\Users\1\Zotero\* (everything located in this folder)
 :: 7z a -tzip "C:\Users\1\Documents\001 - Backup Zotero\Zotero.zip" "C:\Users\1\Zotero\*"
 :: #########################################
-
-7z a -tzip "C:\Users\1\Documents\001 - Backup Zotero\Zotero.zip" "C:\Users\1\Zotero\*"
+7z a -tzip "%zip_buffer_location%" "%zotero_source%"
 
 :: #########################################
 :: copy file to cloud storage folder
@@ -32,13 +65,20 @@ cd C:\Program Files\7-Zip
 :: /y = Suppresses prompting to confirm that you want to overwrite an existing destination file
 :: /s = Copies directories and subdirectories, unless they are empty
 :: #########################################
+xcopy "%zip_buffer_location%" "%cloud_storage%%cloud_path%" /s /y
 
-
-xcopy "C:\Users\1\Documents\001 - Backup Zotero\Zotero.zip" "%cloud_storage%:\Meine Ablage\#003 - Zotero" /s /y
-
+:: #########################################
+:: delete old zotero.zip
+:: #########################################
+del /f "%zip_buffer_location%"
 
 :: #########################################
 :: pause the script to check for possible errors
 :: #########################################
 
+:nein
+echo Abbruch!
+goto ende
+
+:ende
 pause
